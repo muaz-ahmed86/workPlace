b93838ed9f6e2495ca3fbdb13e22a917a2bf6f52
Md Muaz Ahmed <mdmuazahmed86@gmail.com>
2021-12-07 20:31:31 +0600

    One project completed

---
+++ firstProgram/app.js
@@ -0,0 +1,18 @@
+let hour = document.getElementById('hour');
+let minute = document.getElementById('minute');
+let second = document.getElementById('second');
+let progress = document.getElementById('progress-bar');
+
+function showCurrentTIme() {
+    let date = new Date();
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+
+    hour.textContent = hr;
+    minute.textContent = min;
+    second.textContent = sec;
+    progress.style.width = (sec / 60) * 100 + '%'
+}
+
+setInterval(showCurrentTIme, 1000)
